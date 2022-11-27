8439d62b88854e46cdd08fd6aa82a7f51f200c2a
hossam762 <karimaboshamia@gmail.com>
2022-11-27 21:43:36 +0200

    Services DB

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,36 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	
+	
+//	Stage primaryStage;
+//	
+//	public void setScene(Parent sentRoot) {
+//		Parent root = sentRoot;
+//		Scene scene = new Scene(root);
+//		primaryStage.setTitle("Edfa3li Shokran");
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
+//	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+	
 			Scene scene = new Scene(root);
+
 	
 			primaryStage.setTitle("Edfa3li Shokran");
 			primaryStage.setScene(scene);
--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -1,7 +1,28 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 
 public class MainSceneController {
+	
+	@FXML
+	private AnchorPane rootPane;
 
+	// Event Listener on Button.onAction
+	@FXML
+	public void handlerLogin(ActionEvent event) {
+		try {
+			AnchorPane pane = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
+		    rootPane.getChildren().setAll(pane);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
