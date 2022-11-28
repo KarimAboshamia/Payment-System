5898c003349f337d484b23ada678c1bc05cd53ac
habibaahmed11 <habibaahmed3372@gmail.com>
2022-11-29 01:19:53 +0200

    small modification

--- src/application/ServiceManager.java
+++ src/application/ServiceManager.java
@@ -3,20 +3,31 @@ import java.util.*;
 import services.Service;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import services.ServiceCreator;
+import services.Search;
 
 public class ServiceManager {
 	private DBConnection createObj = new DBConnection();
 	Vector<Service> allServices = new Vector<>();
+	private ServiceCreator factory=new ServiceCreator();
+	private Search s=new Search();
+	
 	public void CreateSystemServices() {
 		ResultSet rs=createObj.GetServices();
 		try {
 			while(rs.next()) {
-				Service temp=new Service(rs.getString("Name"));
+				Service temp=factory.createService(rs.getString("Name"));
 				allServices.add(temp);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	//search for service function
+	public Vector<Service> Search(String serviceName){
+		
+		return s.search(allServices, serviceName);
+		
 	}
 }
