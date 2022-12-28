1d4b7bdd2b067330e9a11ae1d39ca818f292eb35
Karim <karimaboshamia@gmail.com>
2022-12-28 05:06:41 +0200

    Add Payment EndPoint

--- src/api/Payment.java
+++ src/api/Payment.java
@@ -8,11 +8,14 @@ import java.util.Vector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import application.Admin;
 import application.AppUser;
+import application.User;
 import managers.ServiceManager;
 import providers.Provider;
 import services.Service;
@@ -31,55 +34,90 @@ public class Payment {
 		//Create all system services, return all providers of the provided service
 		
 		
-		Vector<Service> systemServices = serviceManager.CreateSystemServices();
-		for(int i = 0; i < systemServices.size(); i++) {
-			System.out.println(serviceName);
-			if(systemServices.get(i).getName().toLowerCase().equals(serviceName.toLowerCase())) {
-				for(int j = 0; j < systemServices.get(i).getProviders().size(); j++) {
-					map.put(systemServices.get(i).getProviders().get(j).getProviderName(), systemServices.get(i).getProviders().get(j));					
-				}
+		Service service = searchForService(serviceName);
+		if(service != null) {
+			for(int j = 0; j < service.getProviders().size(); j++) {
+				map.put(service.getProviders().get(j).getProviderName(), service.getProviders().get(j));					
 			}
 			return map;
-
 		}
 		map.put("Provider", null);
 		return map;
-		
 	}
+	
+	
 	
 	@GetMapping(value="/getProviderFormData")
 	@ResponseBody
 	public  Map<String, Map<String, List<String>>> getProviderFormData(@RequestParam String providerName, @RequestParam String serviceName){
 		HashMap<String, Map<String, List<String>>> map = new HashMap<>();
-		Vector<Service> systemServices = serviceManager.CreateSystemServices();
-		for(int i = 0; i < systemServices.size(); i++) {
-			if(systemServices.get(i).getName().toLowerCase().equals(serviceName.toLowerCase())) {
-				for(int j = 0; j < systemServices.get(i).getProviders().size(); j++) {
-					if(systemServices.get(i).getProviders().get(j).getProviderName().toLowerCase().equals(providerName.toLowerCase())) {
-						systemServices.get(i).getProviders().get(j).getTextFieldData();
-						HashMap<String, List<String>> tmp = new HashMap<>();
-						List tmpList = Collections.list(systemServices.get(i).getProviders().get(j).getTextFieldData().elements());
-						tmp.put("TextFields", tmpList);
-						map.put("DropDown", systemServices.get(i).getProviders().get(j).getDropDownData());
-						map.put("TextFields", tmp);
-						return map;
+		Service service = searchForService(serviceName);
+		if(service != null) {
+			for(int j = 0; j < service.getProviders().size(); j++) {
+				if(service.getProviders().get(j).getProviderName().toLowerCase().equals(providerName.toLowerCase())) {
+					service.getProviders().get(j).getTextFieldData();
+					HashMap<String, List<String>> tmp = new HashMap<>();
+					List tmpList = Collections.list(service.getProviders().get(j).getTextFieldData().elements());
+					tmp.put("TextFields", tmpList);
+					map.put("DropDown", service.getProviders().get(j).getDropDownData());
+					map.put("TextFields", tmp);
+					return map;
 
-					}
 				}
-				
 			}
+				
 		}
-
 		map.put("Wrong input", null);
 		return map;
 	}
+
 	
 	@PostMapping(value="/payForProvider")
 	@ResponseBody
-	public  Map<String, String> payForProvider(@RequestParam String providerName, @RequestParam String serviceName){
+	public  Map<String, String> payForProvider(@RequestParam String token, @RequestParam String providerName, @RequestParam String serviceName, @RequestParam int paymentMethod, @RequestBody Map<String,String> inputFields){
 		HashMap<String, String> map = new HashMap<>();
-
+		AppUser user = creation.createUser(token);
+		User normalUser = null;
+		if(user == null) {
+			map.put("Error:", "Wrong token");
+		}
+		if(user.getPermission().equals("0")) {
+			normalUser = (User)user;
+		} else {
+			map.put("State", "Admin can't pay for a provider.");
+		}
+		Service service = searchForService(serviceName);
+		if(service != null) {
+			for(int j = 0; j < service.getProviders().size(); j++) {
+				if(service.getProviders().get(j).getProviderName().toLowerCase().equals(providerName.toLowerCase())) {
+					int textSize = service.getProviders().get(j).getTextFieldData().size();
+					int dropSize = service.getProviders().get(j).getDropDownData().size();
+					HashMap<String,String> textFieldsInput = new HashMap<>();
+					HashMap<String,String> dropDownInput = new HashMap<>();
+					for(int i = 0; i < inputFields.size(); i++) {
+						String key = (String) inputFields.keySet().toArray()[i];
+						String value = (String) inputFields.values().toArray()[i];
+						if(i < service.getProviders().get(j).getTextFieldData().size()) {
+							textFieldsInput.put(key, value);
+							
+						} else {
+							dropDownInput.put(key, value);
+						}
+					}
+					 service.getProviders().get(j).handleUserData(textFieldsInput, dropDownInput, normalUser, serviceName, paymentMethod);
+				}
+			}
+		}
+		
 		return null;
 	}
 
+	public Service searchForService(String serviceName) {
+		Vector<Service> systemServices = serviceManager.CreateSystemServices();
+		for(int i = 0; i < systemServices.size(); i++) {
+			if(systemServices.get(i).getName().toLowerCase().equals(serviceName.toLowerCase())) 
+				return systemServices.get(i);
+		}
+		return null;
+	}
 }
